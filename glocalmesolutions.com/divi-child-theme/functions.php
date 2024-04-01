f7686d45b37fdac4e9385cc1f6268d25ca0a9e57
Allwell Agwu-Okoro <allwell@allwell.dev>
2024-04-01 18:05:46 +0100

    new website development - glocalmesolutions and freedomtopass

---
+++ glocalmesolutions.com/divi-child-theme/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+/* LOADING CSS STYLESHEET FILE */
+add_action( 'wp_enqueue_scripts', 'allwell_register_styles' );
+
+function allwell_register_styles() {
+    $parenthandle = 'divi-style'; 
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(),
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 'divi-child-theme-style', get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version')
+    );
+}
+
