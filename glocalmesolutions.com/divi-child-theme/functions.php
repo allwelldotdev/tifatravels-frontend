932790b4f52b6652810357244848eccde4019193
Allwell Agwu-Okoro <allwell@allwell.dev>
2024-04-02 17:42:39 +0100

    add website css auto update when browser refresh

--- glocalmesolutions.com/divi-child-theme/functions.php
+++ glocalmesolutions.com/divi-child-theme/functions.php
@@ -1,18 +1,23 @@
 <?php
 
 /* LOADING CSS STYLESHEET FILE */
-add_action( 'wp_enqueue_scripts', 'allwell_register_styles' );
+add_action('wp_enqueue_scripts', 'allwell_register_styles');
 
-function allwell_register_styles() {
-    $parenthandle = 'divi-style'; 
+function allwell_register_styles()
+{
+    $parenthandle = 'divi-style';
     $theme = wp_get_theme();
-    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+    wp_enqueue_style(
+        $parenthandle,
+        get_template_directory_uri() . '/style.css',
         array(),
         $theme->parent()->get('Version')
     );
-    wp_enqueue_style( 'divi-child-theme-style', get_stylesheet_uri(),
-        array( $parenthandle ),
-        $theme->get('Version')
+    wp_enqueue_style(
+        'divi-child-theme-style',
+        get_stylesheet_uri(),
+        array($parenthandle),
+        /* $theme->get('Version') */ filemtime(get_stylesheet_directory() . '/style.css')
     );
 }
 
